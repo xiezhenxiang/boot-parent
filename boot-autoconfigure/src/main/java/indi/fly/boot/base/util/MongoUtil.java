3e7a6535641586bd170eb8aabfc967ab3a0e67a5
xiezhenxiang <xzx.555@qq.com>
2019-05-14 19:02:46 +0800

    MysqlUtil

--- boot-autoconfigure/src/main/java/indi/fly/boot/base/util/MongoUtil.java
+++ boot-autoconfigure/src/main/java/indi/fly/boot/base/util/MongoUtil.java
@@ -5,10 +5,18 @@ import com.mongodb.MongoClientOptions;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.UpdateOneModel;
+import com.mongodb.client.model.UpdateOptions;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.bson.BsonValue;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author xiezhenxiang 2019/4/17
@@ -46,7 +54,6 @@ class MongoUtil {
         return client;
     }
 
-
     public static void insertMany(String database, String collection, List<Document> documentList) {
         MongoClient mongoClient = null;
 
@@ -76,6 +83,34 @@ class MongoUtil {
         }
     }
 
+    public static void upsertOne(String database, String collection, Document query, Document doc) {
+        MongoClient mongoClient = null;
+        try {
+            mongoClient = getMongoClient();
+            mongoClient.getDatabase(database).getCollection(collection).updateOne(query, doc, new UpdateOptions().upsert(true));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void upsertManey(String database, String collection, List<Document> ls, List<String> fields) {
+        List<UpdateOneModel<Document>> requests = ls.stream().map(s -> new UpdateOneModel<Document>(
+            new Bson() {
+                @Override
+                public <TDocument> BsonDocument toBsonDocument(Class<TDocument> aClass, CodecRegistry codecRegistry) {
+                    Document doc = new Document();
+                    for (String field : fields) {
+                        doc.append(field, s.get(field));
+                    }
+                    return doc.toBsonDocument(aClass, codecRegistry);
+                }
+            },
+            new Document("$set",s),
+            new UpdateOptions().upsert(true)
+        )).collect(Collectors.toList());
+        client.getDatabase(database).getCollection(collection).bulkWrite(requests);
+    }
+
     public static void updateMany(String database, String collection, Document query, Document doc) {
         try {
             mongoClient = getMongoClient();
@@ -86,17 +121,11 @@ class MongoUtil {
     }
 
     public static MongoCursor<Document> find(String db, String col, Document query, Document sort) {
-        MongoCursor<Document> mongoCursor = null;
-        query = query == null ? new Document() : query;
-        sort = sort == null ? new Document() : sort;
-        try {
-            mongoClient = getMongoClient();
-            FindIterable<Document> findIterable = mongoClient.getDatabase(db).getCollection(col).find(query).sort(sort);
-            mongoCursor = findIterable.iterator();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return mongoCursor;
+        return find(db, col, query, sort, null, null);
+    }
+
+    public static MongoCursor<Document> find(String db, String col, Document query) {
+        return find(db, col, query, null, null, null);
     }
 
     public static MongoCursor<Document> find(String db, String col, Document query, Document sort, Integer pageNo, Integer pageSize) {
@@ -108,7 +137,7 @@ class MongoUtil {
             FindIterable<Document> findIterable = mongoClient.getDatabase(db).getCollection(col).find(query).sort(sort);
             if(pageNo != null)  findIterable.skip(pageNo);
             if(pageSize != null)    findIterable.limit(pageSize);
-            mongoCursor = findIterable.iterator();
+            mongoCursor = findIterable.noCursorTimeout(true).iterator();
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ boot-autoconfigure/src/main/java/indi/fly/boot/base/util/MysqlUtil.java
@@ -0,0 +1,87 @@
+package indi.fly.boot.base.util;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.sql.*;
+import java.util.List;
+
+class MysqlUtil {
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/mysql?characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&useSSL=false";
+    private volatile static Connection con = null;
+
+    private static Connection getConnection() {
+        if (con == null) {
+            synchronized (MysqlUtil.class){
+                if (con == null) {
+                    try {
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+                        con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return con;
+    }
+
+
+    /**
+     * @desc 增删改
+     * @author xiezhenxiang 2019/5/14
+     * @param sql sql语句
+     * @param params 参数
+     **/
+    public static boolean executeUpdate(String sql, List<Object> params)
+            throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement statement;
+        statement = conn.prepareStatement(sql);
+        if (params != null && !params.isEmpty()) {
+            int index = 1;
+            for (int i = 0; i < params.size(); i++) {
+                statement.setObject(index ++, i);
+            }
+        }
+        // 影响数据库的行数
+        int result = statement.executeUpdate();
+        return result > 0;
+    }
+
+    /**
+     * 从数据库中查询数据
+     */
+    public static JSONArray executeQuery(String sql, List<Object> params){
+        Connection conn = getConnection();
+        JSONArray arr = new JSONArray();
+        PreparedStatement statement;
+        try {
+            int index = 1;
+            statement = conn.prepareStatement(sql);
+            if (params != null && !params.isEmpty()) {
+                for (Object para : params) {
+                    statement.setObject(index ++, para);
+                }
+            }
+            ResultSet resultSet = statement.executeQuery();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int cols_len = metaData.getColumnCount();
+            while (resultSet.next()) {
+                JSONObject obj = new JSONObject();
+                for (int i = 0; i < cols_len; i++) {
+                    String cols_name = metaData.getColumnName(i + 1);
+                    Object cols_value = resultSet.getObject(cols_name);
+                    obj.put(cols_name, cols_value);
+                }
+                arr.add(obj);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+}
