38c57a9dc81e0a6e874ed84deef798d35b0c5cc7
解振翔 <1430742291@qq.com>
2019-06-11 18:20:05 +0800

    - -

--- boot-autoconfigure/src/main/java/indi/shine/boot/base/model/handler/StrListHandler.java
+++ boot-autoconfigure/src/main/java/indi/shine/boot/base/model/handler/StrListHandler.java
@@ -1,6 +1,6 @@
 package indi.shine.boot.base.model.handler;
 import com.google.gson.reflect.TypeToken;
-import indi.shine.boot.base.util.JsonUtil;
+import indi.shine.boot.base.util.JSONUtil;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
@@ -13,28 +13,28 @@ import java.util.List;
 public class StrListHandler extends BaseTypeHandler<List<String>> {
     @Override
     public void setNonNullParameter(PreparedStatement preparedStatement, int i, List<String> strings, JdbcType jdbcType) throws SQLException {
-        String param = JsonUtil.toJson(strings);
+        String param = JSONUtil.toJson(strings);
         preparedStatement.setString(i, param);
     }
 
     @Override
     public List<String> getNullableResult(ResultSet resultSet, String columnName) throws SQLException {
         String str = resultSet.getString(columnName);
-        List<String> inputBean = JsonUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
+        List<String> inputBean = JSONUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
         return inputBean;
     }
 
     @Override
     public List<String> getNullableResult(ResultSet resultSet, int i) throws SQLException {
         String str = resultSet.getString(i);
-        List<String> inputBean = JsonUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
+        List<String> inputBean = JSONUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
         return inputBean;
     }
 
     @Override
     public List<String> getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
         String str = callableStatement.getString(i);
-        List<String> inputBean = JsonUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
+        List<String> inputBean = JSONUtil.fromJson(str, new TypeToken<List<String>>(){}.getType());
         return inputBean;
     }
 }
