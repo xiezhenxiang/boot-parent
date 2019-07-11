9aeae95a8b8f77002f8ae3a620f85aa7d1936d95
解振翔 <1430742291@qq.com>
2019-07-11 16:54:34 +0800

    - -

--- boot-autoconfigure/src/main/java/indi/shine/boot/base/util/MongoUtil.java
+++ boot-autoconfigure/src/main/java/indi/shine/boot/base/util/MongoUtil.java
@@ -29,72 +29,62 @@ class MongoUtil {
 
 
     public static MongoCursor<Document> find(String db, String col, Document query, Document sort) {
+
         return find(db, col, query, sort, null, null);
     }
 
     public static MongoCursor<Document> find(String db, String col, Document query) {
+
         return find(db, col, query, null, null, null);
     }
 
     public static MongoCursor<Document> find(String db, String col, Document query, Document sort, Integer pageNo, Integer pageSize) {
+
         getMongoClient();
         MongoCursor<Document> mongoCursor = null;
         query = query == null ? new Document() : query;
         sort = sort == null ? new Document() : sort;
-        try {
-            FindIterable<Document> findIterable = client.getDatabase(db).getCollection(col).find(query).sort(sort);
-            if(pageNo != null && pageSize != null) {
-                pageNo = (pageNo - 1) * pageSize;
-                findIterable.skip(pageNo);
-                findIterable.limit(pageSize);
-            }
-            mongoCursor = findIterable.noCursorTimeout(true).iterator();
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        FindIterable<Document> findIterable = client.getDatabase(db).getCollection(col).find(query).sort(sort);
+        if(pageNo != null && pageSize != null) {
+            pageNo = (pageNo - 1) * pageSize;
+            findIterable.skip(pageNo);
+            findIterable.limit(pageSize);
         }
+        mongoCursor = findIterable.noCursorTimeout(true).iterator();
         return mongoCursor;
     }
 
     public static void insertMany(String database, String collection, List<Document> documentList) {
+
         getMongoClient();
         if (documentList == null || documentList.isEmpty()) {
             return;
         }
-        try {
-            client.getDatabase(database).getCollection(collection).insertMany(documentList);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        client.getDatabase(database).getCollection(collection).insertMany(documentList);
     }
 
     public static void insertOne(String database, String collection, Document doc) {
+
         getMongoClient();
-        try {
-            client.getDatabase(database).getCollection(collection).insertOne(doc);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        client.getDatabase(database).getCollection(collection).insertOne(doc);
     }
 
     public static void updateOne(String database, String collection, Document query, Document doc) {
+
         getMongoClient();
-        try {
-            client.getDatabase(database).getCollection(collection).updateOne(query, new Document("$set", doc));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        client.getDatabase(database).getCollection(collection).updateOne(query, new Document("$set", doc));
     }
 
     public static void upsertOne(String database, String collection, Document query, Document doc) {
+
         getMongoClient();
-        try {
-            client.getDatabase(database).getCollection(collection).updateOne(query, doc, new UpdateOptions().upsert(true));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        client.getDatabase(database).getCollection(collection).updateOne(query, doc, new UpdateOptions().upsert(true));
     }
 
     public static void upsertMany(String database, String collection, List<Document> ls, String... fieldArr) {
+
         getMongoClient();
         List<UpdateOneModel<Document>> requests = ls.stream().map(s -> new UpdateOneModel<Document>(
                 new Bson() {
@@ -110,58 +100,53 @@ class MongoUtil {
                 new Document("$set",s),
                 new UpdateOptions().upsert(true)
         )).collect(Collectors.toList());
+
         client.getDatabase(database).getCollection(collection).bulkWrite(requests);
     }
 
     public static void updateMany(String database, String collection, Document query, Document doc) {
+
         getMongoClient();
-        try {
-            client.getDatabase(database).getCollection(collection).updateMany(query, new Document("$set", doc));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        client.getDatabase(database).getCollection(collection).updateMany(query, new Document("$set", doc));
     }
 
     public static Long count(String db, String col, Document query){
-        long count = 0L;
-        MongoClient mongoClient;
-        try {
-            mongoClient = getMongoClient();
-            count = mongoClient.getDatabase(db).getCollection(col).count(query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return count;
+
+        getMongoClient();
+        return client.getDatabase(db).getCollection(col).count(query);
     }
 
     public static List<Document> getIndex(String db, String col) {
+
         getMongoClient();
         List<Document> indexLs = new ArrayList<>();
         MongoCursor<Document> cursor = client.getDatabase(db).getCollection(col).listIndexes().iterator();
         cursor.forEachRemaining(s -> indexLs.add((Document) s.get("key")));
+
         return indexLs;
     }
 
     public static void delete(String db, String col, Document query){
+
         getMongoClient();
-        try {
-            client.getDatabase(db).getCollection(col).deleteMany(query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        client.getDatabase(db).getCollection(col).deleteMany(query);
     }
 
     public static void createIndex(String db, String col, List<Document> index) {
+
+        getMongoClient();
+
         if (index.isEmpty()) {
             return;
         }
-        getMongoClient();
+
         List<IndexModel> ls = index.stream().map(IndexModel::new).collect(toList());
         client.getDatabase(db).getCollection(col).createIndexes(ls);
     }
 
 
     public static synchronized void copyDataBase(String fromDbName, String toDbName) {
+
         getMongoClient();
         MongoIterable<String> colNames = client.getDatabase(fromDbName).listCollectionNames();
 
@@ -196,23 +181,27 @@ class MongoUtil {
         if (client == null) {
             synchronized (MongoUtil.class){
                 if (client == null) {
-                    try {
-                        MongoClientOptions options = MongoClientOptions.builder()
-                                .connectionsPerHost(20).minConnectionsPerHost(1)
-                                .maxConnectionIdleTime(0).maxConnectionLifeTime(0)
-                                .connectTimeout(30000).socketTimeout(120000).build();
-                        String[] urls = MONGODB_URL.split(",");
-                        List<ServerAddress> urlList = new ArrayList<>();
-                        for (String url : urls) {
-                            urlList.add(new ServerAddress(url, MONGODB_PORT));
-                        }
-                        client = new MongoClient(urlList, options);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+
+                    MongoClientOptions options = MongoClientOptions.builder()
+                            .connectionsPerHost(20)
+                            .minConnectionsPerHost(1)
+                            .maxConnectionIdleTime(0)
+                            .maxConnectionLifeTime(0)
+                            .connectTimeout(30000)
+                            .socketTimeout(120000)
+                            .build();
+
+                    String[] urls = MONGODB_URL.split(",");
+                    List<ServerAddress> urlList = new ArrayList<>();
+                    for (String url : urls) {
+                        urlList.add(new ServerAddress(url, MONGODB_PORT));
                     }
+
+                    client = new MongoClient(urlList, options);
                 }
             }
         }
+
         return client;
     }
 }
