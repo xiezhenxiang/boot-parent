835ac29eccd77e5ae86b2d06f4a34cfa7bb861b5
解振翔 <1430742291@qq.com>
2019-12-25 14:11:36 +0800

    es Util

--- boot-autoconfigure/src/main/java/indi/shine/boot/base/util/JacksonUtil.java
+++ boot-autoconfigure/src/main/java/indi/shine/boot/base/util/JacksonUtil.java
@@ -8,6 +8,7 @@ import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +17,9 @@ import java.util.Map;
  * Jackson util
  * 1、obj need private and set/get；
  * 2、do not support inner class；
- * @author xuxueli 2015-9-25 18:02:56
+ * @author xiezhenxiang 2019-12-25 18:02:56
  */
 public class JacksonUtil {
-	private static Logger logger = LoggerFactory.getLogger(JacksonUtil.class);
 
 	private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 	public static ObjectMapper getInstance() {
@@ -31,71 +31,64 @@ public class JacksonUtil {
 
 	/**
 	 * bean、array、List、Map --> json
-	 * @param obj obj
-	 * @return json string
 	 */
 	public static String writeValueAsString(Object obj) {
 		try {
 			return getInstance().writeValueAsString(obj);
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
 	/**
 	 * string --> bean、Map、List(array)
-	 * @return obj
 	 */
 	public static <T> T readValue(String jsonStr, Class<T> clazz) {
 		try {
 			return getInstance().readValue(jsonStr, clazz);
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
 	/**
 	 * string --> List<Bean>...
-	 * @param jsonStr
-	 * @param parametrized
-	 * @param parameterClasses
-	 * @param <T>
-	 * @return
 	 */
 	public static <T> T readValue(String jsonStr, Class<?> parametrized, Class<?>... parameterClasses) {
 		try {
 			JavaType javaType = getInstance().getTypeFactory().constructParametricType(parametrized, parameterClasses);
 			return getInstance().readValue(jsonStr, javaType);
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
 	public static <T> T readValueRefer(String jsonStr, TypeReference type) {
 		try {
 			return getInstance().readValue(jsonStr, new TypeReference<T>() { });
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
 		}
-		return null;
+	}
+
+	/**
+	 * obj --> bean、Map、List(array)
+	 * @return obj
+	 */
+	public static <T> T convertValue(Object obj, Class<T> clazz) {
+		return readValue(writeValueAsString(obj), clazz);
 	}
 
 	public static void main(String[] args) {
-		try {
-			Map<String, Object> map = new HashMap<>();
-			map.put("aaa", "111");
-			map.put("bbb", 222);
-			String json = writeValueAsString(map);
-			System.out.println(json);
-			System.out.println(readValue(json, Map.class));
-			json = writeValueAsString(Lists.newArrayList(map));
-			List<Map<String, Object>> ls = readValue(json, List.class, Map.class);
-			System.out.println(writeValueAsString(ls));
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("aaa", "111");
+		map.put("bbb", 222);
+		String json = writeValueAsString(map);
+		System.out.println(json);
+		System.out.println(readValue(json, Map.class));
+		json = writeValueAsString(Lists.newArrayList(map));
+		List<Map<String, Object>> ls = readValue(json, List.class, Map.class);
+		System.out.println(writeValueAsString(ls));
 	}
 }
